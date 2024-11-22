c2bed9fa9927d9e01cca89f7f4375f928d76aa4f
Lucikelly <lucikelly2001@gmail.com>
2024-11-22 15:24:29 -0300

    Feat: Controller, routes and schema for sessions

---
+++ src/modules/users/controllers/SessionControllers.ts
@@ -0,0 +1,16 @@
+import SessionUserService from "../services/SessionUserService";
+import { Request, Response } from "express";
+
+export default class SessionControllers {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { email, password } = request.body;
+    const createSession = new SessionUserService();
+
+    const userToken = await createSession.execute({
+      email,
+      password,
+    });
+
+    return response.json(userToken)
+  }
+}
