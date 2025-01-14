26ae695b2b49a048c74c1700e5d3c905a93442ed
Lucikelly <lucikelly2001@gmail.com>
2025-01-14 15:49:38 -0300

    Feat: Profile Service, Controllers and Routes

---
+++ src/modules/users/schemas/UpdateUserSchema.ts
@@ -0,0 +1,14 @@
+import { celebrate, Joi, Segments } from "celebrate";
+
+export const UpdateUserSchema = celebrate({
+  [Segments.BODY]: {
+    name: Joi.string(),
+    email: Joi.string().email(),
+    old_password: Joi.string(),
+    password: Joi.string().optional(),
+    passwordConfimation: Joi.string().valid(Joi.ref('password')).when('password', {
+      is: Joi.exist(),
+      then: Joi.required(),
+    }),
+  },
+});
