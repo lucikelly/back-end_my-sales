77267697b6ddc627fc9e2e7ab1305cf426455100
Lucikelly <lucikelly2001@gmail.com>
2025-01-16 16:38:49 -0300

    Feat: entity orders_products

--- src/modules/orders/database/entities/Order.ts
+++ src/modules/orders/database/entities/Order.ts
@@ -1,5 +1,6 @@
 import { Customer } from "@modules/customers/database/entities/Customer";
-import { CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { OrdersProducts } from "./OrdersProducts";
 
 
 @Entity('orders')
@@ -10,6 +11,11 @@ export class Order {
   @ManyToOne(() => Customer)
   @JoinColumn({ name: 'customer_id' })
   customer: Customer;
+
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true,
+  })
+  order_products: OrdersProducts[];
 
   @CreateDateColumn()
   created_at: Date;
---
+++ src/modules/orders/database/entities/OrdersProducts.ts
@@ -0,0 +1,29 @@
+import { Product } from "@modules/products/database/entities/Product";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Order } from "./Order";
+
+@Entity('orders_products')
+export class OrdersProducts {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Order, order => order.order_products)
+  @JoinColumn({name: 'order_id' })
+  order: Order;
+
+  @ManyToOne(() => Product, product => product.order_products)
+  @JoinColumn({name: 'product_id'})
+  product: Product
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quatity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
